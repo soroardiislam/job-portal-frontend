fc8ab355918e0989bd24bf55dc3febdf8af082a0
soroardiislam <mdsoroardi602828@gmail.com>
2025-11-18 10:38:05 +0600

    code update

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -26,36 +26,28 @@ const Login = () => {
         setInput({ ...input, [e.target.name]: e.target.value });
     }
 
-   const submitHandler = async (e) => {
-    e.preventDefault();
-    try {
-        dispatch(setLoading(true));
-
-        const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
-            headers: {
-                "Content-Type": "application/json"
-            },
-            withCredentials: true,
-        });
-
-        if (res.data.success) {
-
-            // 🔥 TOKEN SAVE HERE
-            localStorage.setItem("token", res.data.token);
-
-            dispatch(setUser(res.data.user));
-            toast.success(res.data.message);
-            navigate("/");
-        }
-
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            dispatch(setLoading(true));
+            const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                withCredentials: true,
+            });
+            if (res.data.success) {
+                dispatch(setUser(res.data.user));
+                navigate("/");
+                toast.success(res.data.message);
+            }
         } catch (error) {
-           console.log(error);
-          toast.error(error.response.data.message);
+            console.log(error);
+            toast.error(error.response.data.message);
         } finally {
-             dispatch(setLoading(false));
+            dispatch(setLoading(false));
         }
-     };
-
+    }
     useEffect(()=>{
         if(user){
             navigate("/");
