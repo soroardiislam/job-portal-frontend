5225d9391167f842f3f99446f40e6531d662c352
soroardiislam <mdsoroardi602828@gmail.com>
2025-12-24 23:17:40 +0600

    admin

--- src/components/admin/AdminJobsTable.jsx
+++ src/components/admin/AdminJobsTable.jsx
@@ -1,83 +1,99 @@
-import React, { useEffect, useState } from 'react'
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
-import { Avatar, AvatarImage } from '../ui/avatar'
-import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 // FIX: Add Trash2 to the imports list
-import { Edit2, Eye, MoreHorizontal, Trash2 } from 'lucide-react' 
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import { Edit2, Eye, MoreHorizontal, Trash2 } from "lucide-react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const AdminJobsTable = () => { 
-    const {allAdminJobs, searchJobByText} = useSelector(store=>store.job);
+const AdminJobsTable = () => {
+  const { allAdminJobs, searchJobByText } = useSelector((store) => store.job);
 
-    const [filterJobs, setFilterJobs] = useState(allAdminJobs);
-    const navigate = useNavigate();
+  const [filterJobs, setFilterJobs] = useState(allAdminJobs);
+  const navigate = useNavigate();
 
-    const handleDeleteJob = (id) =>{
-        console.log(id);
-        axios.delete(`https://job-portal-backend-mvc2.onrender.com/api/v1/job/delete/${id}`)
-        .then(res =>console.log(res.data))
-        .catch(err => console.log(err))
-    }
+  const handleDeleteJob = (id) => {
+    console.log(id);
+    axios
+      .delete(
+        `https://job-portal-backend-mvc2.onrender.com/api/v1/job/delete/${id}`
+      )
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
 
-    useEffect(()=>{ 
-        // console.log('called');
-        const filteredJobs = allAdminJobs.filter((job)=>{
-            if(!searchJobByText){
-                return true;
-            };
-            return job?.title?.toLowerCase().includes(searchJobByText.toLowerCase()) || job?.company?.name.toLowerCase().includes(searchJobByText.toLowerCase());
+  useEffect(() => {
+    // console.log('called');
+    const filteredJobs = allAdminJobs.filter((job) => {
+      if (!searchJobByText) {
+        return true;
+      }
+      return (
+        job?.title?.toLowerCase().includes(searchJobByText.toLowerCase()) ||
+        job?.company?.name.toLowerCase().includes(searchJobByText.toLowerCase())
+      );
+    });
+    setFilterJobs(filteredJobs);
+  }, [allAdminJobs, searchJobByText]);
+  return (
+    <div>
+      <Table>
+        <TableCaption>A list of your recent  posted jobs</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Company Name</TableHead>
+            <TableHead>Role</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead className="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {filterJobs?.map((job) => (
+            <tr key={job._id}>
+              <TableCell>{job?.company?.name}</TableCell>
+              <TableCell>{job?.title}</TableCell>
+              <TableCell>{job?.createdAt.split("T")[0]}</TableCell>
+              <TableCell className="text-right cursor-pointer">
+                <Popover>
+                  <PopoverTrigger>
+                    <MoreHorizontal />
+                  </PopoverTrigger>
+                  <PopoverContent className="w-32">
+                    <div
+                      onClick={() => handleDeleteJob(job._id)}
+                      className="flex items-center gap-2 w-fit cursor-pointer text-red-600 hover:bg-red-50/50 p-1 rounded"
+                    >
+                      <Trash2 className="w-4" />
+                      <span>Delete</span>
+                    </div>
+                    <div
+                      onClick={() =>
+                        navigate(`/admin/jobs/${job._id}/applicants`)
+                      }
+                      className="flex items-center w-fit gap-2 cursor-pointer mt-2"
+                    >
+                      <Eye className="w-4" />
+                      <span>Applicants</span>
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              </TableCell>
+            </tr>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
 
-        });
-        setFilterJobs(filteredJobs);
-    },[allAdminJobs,searchJobByText])
-    return (
-        <div>
-            <Table>
-                <TableCaption>A list of your recent  posted jobs</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Company Name</TableHead>
-                        <TableHead>Role</TableHead>
-                        <TableHead>Date</TableHead>
-                        <TableHead className="text-right">Action</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {
-                        filterJobs?.map((job) => (
-                            <tr key={job._id}> 
-                                <TableCell>{job?.company?.name}</TableCell>
-                                <TableCell>{job?.title}</TableCell>
-                                <TableCell>{job?.createdAt.split("T")[0]}</TableCell>
-                                <TableCell className="text-right cursor-pointer">
-                                    <Popover>
-                                        <PopoverTrigger><MoreHorizontal /></PopoverTrigger>
-                                        <PopoverContent className="w-32">
-                                            <div 
-                                                onClick={() => handleDeleteJob(job._id)} 
-                                                className='flex items-center gap-2 w-fit cursor-pointer text-red-600 hover:bg-red-50/50 p-1 rounded' 
-                                                >
-                                             
-                                                <Trash2 className='w-4' /> 
-                                                <span>Delete</span>
-                                            </div>
-                                            <div onClick={()=> navigate(`/admin/jobs/${job._id}/applicants`)} className='flex items-center w-fit gap-2 cursor-pointer mt-2'>
-                                                <Eye className='w-4'/>
-                                                <span>Applicants</span>
-                                            </div>
-                                        </PopoverContent>
-                                    </Popover>
-                                </TableCell>
-                            </tr>
-
-                        ))
-                    }
-                </TableBody>
-            </Table>
-        </div>
-    )
-}
-
-export default AdminJobsTable
+export default AdminJobsTable;
--- src/components/admin/ProtectedRoute.jsx
+++ src/components/admin/ProtectedRoute.jsx
@@ -2,21 +2,17 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-const ProtectedRoute = ({children}) => {
-    const {user} = useSelector(store=>store.auth);
+const ProtectedRoute = ({ children }) => {
+  const { user } = useSelector((store) => store.auth);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        if(user === null || user.role !== 'recruiter'){
-            navigate("/");
-        }
-    },[]);
+  useEffect(() => {
+    if (user === null || user.role !== "recruiter") {
+      navigate("/");
+    }
+  }, []);
 
-    return (
-        <>
-        {children}
-        </>
-    )
+  return <>{children}</>;
 };
 export default ProtectedRoute;
