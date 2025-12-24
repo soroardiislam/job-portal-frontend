d9fbc2a5323c5a4056f4b71fd6ff99fba8c6730b
soroardiislam <mdsoroardi602828@gmail.com>
2025-12-24 23:19:58 +0600

    components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react'
-import Navbar from './shared/Navbar'
-import HeroSection from './HeroSection'
-import CategoryCarousel from './CategoryCarousel'
-import LatestJobs from './LatestJobs'
-import Footer from './shared/Footer'
-import useGetAllJobs from '@/hooks/useGetAllJobs'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect } from "react";
+import Navbar from "./shared/Navbar";
+import HeroSection from "./HeroSection";
+import CategoryCarousel from "./CategoryCarousel";
+import LatestJobs from "./LatestJobs";
+import Footer from "./shared/Footer";
+import useGetAllJobs from "@/hooks/useGetAllJobs";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   useGetAllJobs();
-  const { user } = useSelector(store => store.auth);
+  const { user } = useSelector((store) => store.auth);
   const navigate = useNavigate();
   useEffect(() => {
-    if (user?.role === 'recruiter') {
+    if (user?.role === "recruiter") {
       navigate("/admin/companies");
     }
   }, []);
@@ -25,7 +25,7 @@ const Home = () => {
       <LatestJobs />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
